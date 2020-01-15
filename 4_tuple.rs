1ef909ada2d1593b2bdb8a3ed779c94bda32c08c
liuzhanliang <zhanliangliu@qq.com>
2020-01-15 14:21:30 +0800

    add 4_tuple.rs

---
+++ 4_tuple.rs
@@ -0,0 +1,25 @@
+fn reverse(pair: (i32, bool)) -> (bool, i32) {
+    let (integer, boolean) = pair;
+    (boolean, integer)
+}
+
+#[derive(Debug)]
+struct Matrix(f32, f32, f32, f32);
+
+fn main() {
+    let long_tuple = (
+        1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true,
+    );
+    println!("long tuple first value: {}", long_tuple.0);
+    println!("long tuple third value: {}", long_tuple.2);
+
+    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    println!("tuple of tuples: {:?}", tuple_of_tuples);
+
+    let pair = (1, true);
+    println!("pair is {:?}", pair);
+    println!("the reversed pair is {:?}", reverse(pair));
+
+    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
+    println!("{:?}", matrix);
+}
